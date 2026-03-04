590ade52f2666a11d82b323798473bc451066cd7
maheshray1221 <mr5832388@gmail.com>
2026-03-04 13:27:16 +0530

    add sign-in form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,124 @@
-import React from 'react'
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import * as z from "zod";
+import { useDebounceCallback } from "usehooks-ts";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import axios, { AxiosError } from "axios";
+import { signUpSchema } from "@/schemas/signUpSchema";
+import { ApiResponse } from "@/types/apiResponse";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2, LoaderPinwheel, LoaderPinwheelIcon } from "lucide-react";
+import { InputGroup, InputGroupAddon } from "@/components/ui/input-group";
+import { signInSchema } from "@/schemas/signInSchema";
+import { signIn } from "next-auth/react";
 
 export default function page() {
+  const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  // zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    // resolver me use ho raha hai zodResoolver
+    // and zodResolover ek schema mangta hai
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    const response = await signIn("credential", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+    if (response?.error) {
+      toast.error("Log in failed ! Incorrect username or password");
+    }
+    if (response?.url) {
+      router.replace("/dashboard");
+    }
+  };
   return (
-    <div>Sign in page</div>
-  )
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Message
+          </h1>
+          <p className="mb-4">Sign in to start your anonymous adventure</p>
+        </div>
+        {/* start ower form */}
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+            <Controller
+              name="identifier"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-demo-title">
+                    Email/Username
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    id="form-rhf-demo-title"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Email/username"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="form-rhf-demo-title">
+                    Password
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    id="form-rhf-demo-title"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Password"
+                    type="password"
+                    autoComplete="off"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+          <Button
+            type="submit"
+            variant="outline"
+            size="sm"
+            disabled={isSubmitting}
+          >
+            Sign-in
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
